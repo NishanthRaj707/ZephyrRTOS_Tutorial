2e910cea8cecdc5277c0c620ba12cf4216675125
NishanthRaj707 <jpnishanthraj@gmail.com>
2026-08-22 11:22:02 +0530

    uart finally

--- uart/build/zephyr/zephyr.dts.d
+++
@@ -1,7 +0,0 @@
-empty_file.o: /home/bava/zephyrproject/zephyr/misc/empty_file.c \
- /home/bava/zephyrproject/zephyr/boards/qemu/cortex_m3/qemu_cortex_m3.dts \
- /home/bava/zephyrproject/zephyr/dts/arm/ti/lm3s6965.dtsi \
- /home/bava/zephyrproject/zephyr/dts/arm/armv7-m.dtsi \
- /home/bava/zephyrproject/zephyr/dts/common/skeleton.dtsi \
- /home/bava/Desktop/ZephyrRTOS_Tutorial/uart/app.overlay \
- /home/bava/zephyrproject/zephyr/include/zephyr/dt-bindings/gpio/gpio.h
